3e53d235ed0eb9492efbded2d6e4fd4a39a1ba42
24-Muhammad-Ikhsan-X-PPLG-2 <ikhsanm181209@gmail.com>
2026-05-18 20:26:02 +0700

    fix: halaman services tampilan melebihi width device

--- src/features/services/components/About.tsx
+++ src/features/services/components/About.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { MoveRight, Palette, Smartphone } from "lucide-react";
 import CustomWebDevelopment from "./CustomWebDevelopment";
 import UiUxDesign from "./UiUxDesign";
 import AppDevelopment from "./AppDevelopment";
 
 const About = () => {
   return (
-    <section className="min-h-screen flex flex-col items-center gap-6 px-4 sm:px-6 lg:px-10 py-10 bg-[#131b2e]">
+    <section className="min-h-screen flex flex-col items-center gap-6 px-4 sm:px-6 lg:px-10 py-10 bg-[#131b2e] overflow-hidden">
       <CustomWebDevelopment />
       <UiUxDesign />
       <AppDevelopment />
