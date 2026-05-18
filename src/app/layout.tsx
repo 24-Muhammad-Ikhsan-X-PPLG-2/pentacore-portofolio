d09d8bce20a49fe86a17b23e89007835a97f872d
24-Muhammad-Ikhsan-X-PPLG-2 <ikhsanm181209@gmail.com>
2026-05-18 21:15:43 +0700

    feat: SEO Optimized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,25 @@ const InterFont = Inter({
 
 export const metadata: Metadata = {
   title: appName,
-  description: "PentaCore",
+  description:
+    "We are a professional web development and web design agency that builds modern, responsive, and SEO-optimized websites for UMKM, businesses, and educational institutions. We focus on creating fast, user-friendly, and high-performing websites through web development, UI/UX design, and optimization to help clients grow their digital presence, increase visibility, and convert more customers online.",
+  openGraph: {
+    title: appName,
+    description:
+      "We are a professional web development and web design agency that builds modern, responsive, and SEO-optimized websites for UMKM, businesses, and educational institutions. We focus on creating fast, user-friendly, and high-performing websites through web development, UI/UX design, and optimization to help clients grow their digital presence, increase visibility, and convert more customers online.",
+    url: "https://pentacore-labs.vercel.app/",
+    siteName: "Pentacore Labs",
+    images: [
+      {
+        url: "/banner.webp",
+        width: 1200,
+        height: 630,
+        alt: "Pentacore Labs",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
